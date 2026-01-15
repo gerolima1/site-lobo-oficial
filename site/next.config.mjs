9a1d27e48a9d2922f9c3546eaf14da64c1eb399c
gerolima1 <silvageronicio@gmail.com>
2026-01-15 15:51:07 -0300

    Update next.config.mjs

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Isso desativa o uso de múltiplos processos no build
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  },
+  // Desativa verificação de tipos e lint no build (economiza muita RAM)
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
